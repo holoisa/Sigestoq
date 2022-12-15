10352311cd3e27ad8bd4be994dec2adae0c5a267
Ana Heloísa <112425345+holoisa@users.noreply.github.com>
2022-12-15 10:07:07 -0300

    Update sigestoq.sql

--- sigestoq.sql
+++ sigestoq.sql
@@ -1,40 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 30-Out-2022 às 23:38
--- Versão do servidor: 10.4.24-MariaDB
--- versão do PHP: 8.1.6
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Banco de dados: `sigestoq`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `categoria`
---
-
 CREATE TABLE `categoria` (
   `IDcategoria` int(11) NOT NULL,
   `categoria_nome` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Extraindo dados da tabela `categoria`
---
 
 INSERT INTO `categoria` (`IDcategoria`, `categoria_nome`) VALUES
 (1, 'Verduras e Legumes'),
@@ -47,11 +14,7 @@ INSERT INTO `categoria` (`IDcategoria`, `categoria_nome`) VALUES
 (8, 'Tempero'),
 (9, 'Não Perecível');
 
--- --------------------------------------------------------
 
---
--- Estrutura da tabela `estoque`
---
 
 CREATE TABLE `estoque` (
   `IDestoque` int(11) NOT NULL,
@@ -61,18 +24,7 @@ CREATE TABLE `estoque` (
   `produto_peso` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Extraindo dados da tabela `estoque`
---
 
-INSERT INTO `estoque` (`IDestoque`, `produto_nome`, `produto_quantidade`, `produto_validade`, `produto_peso`) VALUES
-(1, 'Arroz Branco', 2, '0000-00-00', 4.2);
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `funcionario`
---
 
 CREATE TABLE `funcionario` (
   `IDfuncionario` int(11) NOT NULL,
@@ -81,18 +33,6 @@ CREATE TABLE `funcionario` (
   `funcionario_tipo` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Extraindo dados da tabela `funcionario`
---
-
-INSERT INTO `funcionario` (`IDfuncionario`, `IDinstituicao`, `funcionario_nome`, `funcionario_tipo`) VALUES
-(1, 1, 'Rebecca Jemima', 'Comum');
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `instituicao`
---
 
 CREATE TABLE `instituicao` (
   `IDinstituicao` int(11) NOT NULL,
@@ -102,87 +42,22 @@ CREATE TABLE `instituicao` (
   `Local` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Extraindo dados da tabela `instituicao`
---
+
 
 INSERT INTO `instituicao` (`IDinstituicao`, `IDestoque`, `IDfucionario`, `instituicao_nome`, `Local`) VALUES
 (1, 1, NULL, 'Centro Estadual de Educação Profissional', 'Rio Grande do Norte');
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `produto`
---
 
 CREATE TABLE `produto` (
   `IDproduto` int(11) NOT NULL,
   `IDcategoria` int(11) NOT NULL,
   `produto_nome` varchar(255) NOT NULL,
   `produto_quantidade` int(11) NOT NULL,
-  `produto_validade` date NOT NULL,
-  `produto_peso` float NOT NULL
+  `produto_validade` date,
+  `produto_peso` float
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Extraindo dados da tabela `produto`
---
 
 INSERT INTO `produto` (`IDproduto`, `IDcategoria`, `produto_nome`, `produto_quantidade`, `produto_validade`, `produto_peso`) VALUES
 (1, 6, 'Arroz Branco', 2, '2022-12-05', 4.2),
 (2, 5, 'Carne de Sol', 0, '0000-00-00', 9);
-
---
--- Índices para tabelas despejadas
---
-
---
--- Índices para tabela `categoria`
---
-ALTER TABLE `categoria`
-  ADD PRIMARY KEY (`IDcategoria`);
-
---
--- Índices para tabela `estoque`
---
-ALTER TABLE `estoque`
-  ADD PRIMARY KEY (`IDestoque`);
-
---
--- Índices para tabela `funcionario`
---
-ALTER TABLE `funcionario`
-  ADD PRIMARY KEY (`IDfuncionario`);
-
---
--- Índices para tabela `produto`
---
-ALTER TABLE `produto`
-  ADD PRIMARY KEY (`IDproduto`);
-
---
--- AUTO_INCREMENT de tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `categoria`
---
-ALTER TABLE `categoria`
-  MODIFY `IDcategoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
---
--- AUTO_INCREMENT de tabela `estoque`
---
-ALTER TABLE `estoque`
-  MODIFY `IDestoque` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT de tabela `produto`
---
-ALTER TABLE `produto`
-  MODIFY `IDproduto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
